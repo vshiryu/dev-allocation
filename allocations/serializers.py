4ac1164bf3ecd984d46574b1f78f7e113a8ec4ca
Vinicius Shiryu Hagio <viniciusshiryu@outlook.com>
2025-01-18 15:32:57 -0300

    feat: allocation and project validations

--- allocations/serializers.py
+++ allocations/serializers.py
@@ -10,8 +10,16 @@ class AllocationSerializer(serializers.ModelSerializer):
         project = attrs.get('project')
         programmer = attrs.get('programmer')
         required_technologies = project.technologies.all()
+        hours = attrs.get('hours')
 
         if not any(tech in programmer.technologies.all() for tech in required_technologies):
             raise serializers.ValidationError("Programmer must have at least one technology required by the project.")
+        
+        start_date = project.start_date
+        end_date = project.end_date
+        
+        duration = (end_date - start_date).days
+        if hours > duration * 24:
+            raise serializers.ValidationError(f"The hours planned for the project cannot exceed the total available hours between {start_date} and {end_date}.")
         
         return attrs
--- projects/serializers.py
+++ projects/serializers.py
@@ -8,3 +8,12 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = ['id', 'name', 'start_date', 'end_date', 'technologies']
+
+    def validate(self, attrs):
+        print(attrs.get('start_date'))
+        start_date = attrs.get('start_date')
+        end_date = attrs.get('end_date')
+        if (start_date > end_date):
+            raise serializers.ValidationError("Start date cannot be after End date")
+
+        return attrs
